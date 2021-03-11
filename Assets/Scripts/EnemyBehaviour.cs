de38b06615eb3ea95281dc53d1c941332d061af0
Baxi19 <randald1991@gmail.com>
2021-03-10 18:50:48 -0600

    :+1: Movimiento del enemigo

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    Rigidbody2D enemyRb;
+    float timeBeforeChange;
+    public float delay = 2.5f;
+    public float speed = .9f;
+
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyRb.velocity = Vector2.right * speed;
+
+        if(timeBeforeChange < Time.time){
+            speed *= -1;
+            timeBeforeChange = Time.time + delay;
+        }
+    }
+}
