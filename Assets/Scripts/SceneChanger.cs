47ca5875f8dbeb477a03a6e19f9bd3469a61f3e6
Baxi19 <randald1991@gmail.com>
2021-03-21 14:01:17 -0600

    :+1: Condición de victoria && Inicio de segundo nivel

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public static void ChangeSceneTo(string sceneName){
+		SceneManager.LoadScene(sceneName);
+	}
+
+	public static void NextScene(){
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+	}
+}
+
+
